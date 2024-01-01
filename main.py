5003ea517554bd5501ee604c963258724ad01736
vialaky <vialaky@gmail.com>
2024-01-01 17:46:27 +0200

    find all coordinates (update)

--- main.py
+++ main.py
@@ -5,20 +5,13 @@ str_latitude = ''
 str_longitude = ''
 result = []
 
-with open('points.txt', encoding="utf8") as f:
-    text = f.read()
-
-
-# pat = r'\d\d'
 pat = '([0-9]{2}.[0-9]+)[\s,]+([0-9]{2}.[0-9]+)'
 
-
-points = re.findall(pat, text)
+with open('points.txt', encoding="utf8") as f:
+    points = re.findall(pat, f.read())
 
 for point in points:
     # print('lat:', match.group(1), 'long:', match.group(2))
     print(point)
 
 print(len(points))
-
-
