06e133cb052e716ef30e8e39ad583631615443ab
vialaky <vialaky@gmail.com>
2024-01-04 20:09:30 +0200

    Find my location.

--- main.py
+++ main.py
@@ -2,28 +2,63 @@ import re
 import webbrowser
 
 import folium
+# import keyboard
 
 # Import folium MarkerCluster plugin
 from folium.plugins import MarkerCluster
+
 # Import folium MousePosition plugin
-from folium.plugins import MousePosition
+# from folium.plugins import MousePosition
 # Import folium DivIcon plugin
-from folium.features import DivIcon
+# from folium.features import DivIcon
+
+
+from geopy.geocoders import Nominatim  # Подключаем библиотеку
 
 # pat = '[0-9]{2}.[0-9]+[\s,]+[0-9]{2}.[0-9]+'
 pat = '[0-9]{2}.[0-9]+'
-pat1 = r'[0-9]{2}[.][0-9]+'
+# pat1 = r'[0-9]{2}[.][0-9]+'
 
 numbers = []
-locations = []
+points = []
 
 symbol1 = '.'
 symbol2 = ','
 
-with open('points.txt', encoding="utf8") as f:
-    digits = re.findall(pat, f.read())
+geolocator = Nominatim(user_agent="Tester")  # Указываем название приложения (так нужно, да)
 
-# print(digits)
+while True:
+
+    try:
+        address = str(input('Enter your location: \n'))  # Получаем интересующий нас адрес
+        locations = geolocator.geocode(address,
+                                       exactly_one=False)  # Создаем переменную, которая состоит из нужного нам адреса
+        locations_ua = [loc_ua for loc_ua in locations if 'Україна' in loc_ua.address]
+
+        size = len(locations_ua)
+        for i in range(size):
+            print(i + 1, locations_ua[i])
+        if size == 1:
+            location = locations_ua[i]
+            break
+        elif size > 1:
+            print('Enter the number of your place:')
+            num = int(input())
+
+            location = locations_ua[num - 1]
+            print(location)
+            break
+        else:
+            print('!!! No matches. Try again!')
+    except TypeError:
+        print('!!! No matches. Try again!')
+
+print(location.latitude, location.longitude)  # И теперь выводим GPS-координаты нужного нам адреса
+
+my_location = [location.latitude, location.longitude]
+
+with open('message.txt', encoding="utf8") as f:
+    digits = re.findall(pat, f.read())
 
 for item in digits:
     if symbol1 in item:
@@ -31,96 +66,27 @@ for item in digits:
     elif symbol2 in item:
         numbers.append(float(item.replace(',', '.')))
     elif item.isdigit():
-        numbers.append(float(item[:2]+symbol1+item[2:]))
+        numbers.append(float(item[:2] + symbol1 + item[2:]))
 
 for i in range(0, len(numbers), 2):
-    locations.append(list((numbers[i], numbers[i+1])))
-
-# for loc in locations:
-#     print(loc)
-# for number in numbers:
-# numbers = re.sub(pat, pat1, digits)
-#
-# # for point in points_str:
-# #     print(point)
-#
-# print(numbers)
-
-# correct_numbers = re.sub(pat, pat1, )
-
-# numbers = [float(x) for x in digits]
-# print(numbers)
-
-# for item in digits:
-#     if item[2] == ',':
-#         item[2]
-
-# points = list(map(list, points_str))
-
-# points = [[x[0], x[1]] for x in points_str]
-
-# print(points[0])
-
-# for point in points:
-#     print(point)
-#
-#
-# print(points)
-
-# for p in points_str:
-#     print(p)
-
-# print(float(points[0][0]))
-
-# print(points_str)
-
-# for point in points:
-#     # print('lat:', match.group(1), 'long:', match.group(2))
-#     print(point)
-
-# print(len(digits), '/', len(numbers))
-print('Количество точек:', len(locations))
-
-# my_location = [48.737780, 37.584170]    # Kramatorsk
-my_location = [49.052164, 37.349729]    # Izyum
-
-
-
-
+    points.append(list((numbers[i], numbers[i + 1])))
 
 map = folium.Map(
-    location = my_location,
-    zoom_start = 10
+    location=my_location,
+    zoom_start=10
 )
 
 folium.CircleMarker(
     location=my_location,
-    radius=3,
+    radius=4,
     color='red',
-    fill_color='Red'
-    ).add_to(map)
+    fill_color='Red',
+    fill_opacity=0.9
+).add_to(map)
 
 marker_cluster = MarkerCluster().add_to(map)
 
-
-
-# add a red marker to Saint Petersburg
-# create a feature group (создать группу функций)
-# saint_petersburg = folium.map.FeatureGroup()
-
-# style the feature group (стиль группы объектов)
-# saint_petersburg.add_child(
-#     folium.features.CircleMarker(
-#         my_location, radius = 5,
-#         color = 'red', fill_color = 'Red'
-#     )
-# )
-
-# add the feature group to the map  (добавить группу объектов на карту)
-# m.add_child(saint_petersburg)
-
-
-for point in locations:
+for point in points:
     # point = list(point)
     # print(point)
 
@@ -128,7 +94,8 @@ for point in locations:
         location=point,
     ).add_to(marker_cluster)
 
-
 output_file = "map.html"
 map.save(output_file)
 webbrowser.open(output_file, new=2)  # open in new tab
+
+print('Number of locations in message:', len(points))
