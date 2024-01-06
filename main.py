6c85fc61e5db838fb5ba157376c69fa70c1f9a8e
vialaky <vialaky@gmail.com>
2024-01-06 15:43:19 +0200

    Catch extensions.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import re
+import sys
 import webbrowser
 
 import folium
+import geopy
+import requests
 # import keyboard
 
 # Import folium MarkerCluster plugin
@@ -29,35 +32,7 @@ symbol2 = ','
 
 geolocator = Nominatim(user_agent="Tester")  # Указываем название приложения (так нужно, да)
 
-while True:
-
-    try:
-        address = str(input('Enter your location: \n'))  # Получаем интересующий нас адрес
-        locations = geolocator.geocode(address,
-                                       exactly_one=False)  # Создаем переменную, которая состоит из нужного нам адреса
-        locations_ua = [loc_ua for loc_ua in locations if 'Україна' in loc_ua.address]
-
-        size = len(locations_ua)
-        for i in range(size):
-            print(i + 1, locations_ua[i])
-        if size == 1:
-            location = locations_ua[i]
-            break
-        elif size > 1:
-            print('Enter the number of your place:')
-            num = int(input())
-
-            location = locations_ua[num - 1]
-            print(f'Your location is: \n{location}')
-            break
-        else:
-            print('!!! No matches. Try again!')
-    except TypeError:
-        print('!!! No matches. Try again!')
-
-print(location.latitude, location.longitude)  # И теперь выводим GPS-координаты нужного нам адреса
-
-my_location = [location.latitude, location.longitude]
+# print('1')
 
 with open('message.txt', encoding="utf8") as f:
     digits = re.findall(pat, f.read())
@@ -73,34 +48,78 @@ for item in digits:
 for i in range(0, len(numbers), 2):
     points.append(list((numbers[i], numbers[i + 1])))
 
-map = folium.Map(
-    location=my_location,
-    zoom_start=10
-)
+# print(points)
 
-folium.CircleMarker(
-    location=my_location,
-    radius=4,
-    color='red',
-    fill_color='Red',
-    fill_opacity=0.9
-).add_to(map)
+while True:
 
-marker_cluster = MarkerCluster().add_to(map)
+    try:
+        # print('2')
+        address = str(input('Enter your location: \n'))  # Получаем интересующий нас адрес
+        locations = geolocator.geocode(address,
+                                       exactly_one=False)  # Создаем переменную, которая состоит из нужного нам адреса
+        locations_ua = [loc_ua for loc_ua in locations if 'Україна' in loc_ua.address]
 
-for point in points:
+        size = len(locations_ua)
+        for i in range(size):
+            print(i + 1, locations_ua[i])
+        if size == 1:
+            location = locations_ua[i]
+            # break
+        elif size > 1:
+            print('Enter the number of your place:')
+            num = int(input())
 
-    folium.Marker(
-        location=point,
-    ).add_to(marker_cluster)
+            location = locations_ua[num - 1]
+            print(f'Your location is: \n{location}')
+            # break
+        else:
+            print('!!! No matches. Try again!')
 
-    distance2point[points.index(point)+1] = round(distance.distance(my_location, tuple(point)).km, 1)
 
-output_file = "map.html"
-map.save(output_file)
-webbrowser.open(output_file, new=2)  # open in new tab
+        # print('3')
 
-print('Number of locations in message:', len(points))
+        print(location.latitude, location.longitude)  # И теперь выводим GPS-координаты нужного нам адреса
 
-print('Min distance:', min(distance2point.values()))
+        my_location = [location.latitude, location.longitude]
+
+
+
+
+
+        map = folium.Map(
+            location=my_location,
+            zoom_start=10
+        )
+
+        folium.CircleMarker(
+            location=my_location,
+            radius=4,
+            color='red',
+            fill_color='Red',
+            fill_opacity=0.9
+        ).add_to(map)
+
+        marker_cluster = MarkerCluster().add_to(map)
+
+        for point in points:
+
+            folium.Marker(
+                location=point,
+            ).add_to(marker_cluster)
+
+            distance2point[points.index(point)+1] = round(distance.distance(my_location, tuple(point)).km, 1)
+
+        output_file = "map.html"
+        map.save(output_file)
+        webbrowser.open(output_file, new=2)  # open in new tab
+
+        print('Number of locations in message:', len(points))
+
+        print('Min distance:', min(distance2point.values()))
+        sys.exit()
+
+    except TypeError:
+        print('!!! No matches. Try again!')
+    except geopy.exc.GeocoderUnavailable: # requests.exceptions.ConnectionError:
+        print('Bad connect. Restart...')
 
