368ab387141f11e564018668d76f286e334dbc5c
vialaky <vialaky@gmail.com>
2024-01-12 13:19:13 +0200

    Stable.

--- main.py
+++ main.py
@@ -22,10 +22,11 @@ symbol2 = ','
 geolocator = Nominatim(user_agent="points_on_map")     # Geocoder’s initialization)
 
 
-# Reading the message
+# Read the message and finding the elements according to the pattern
 with open('message.txt', encoding="utf8") as f:
     digits = re.findall(pat, f.read())
 
+# Bring found elements back to form "XX.XXXXXXX XX.XXXXXXX"
 for item in digits:
     if symbol1 in item:
         numbers.append(float(item))
@@ -34,6 +35,7 @@ for item in digits:
     elif item.isdigit():
         numbers.append(float(item[:2] + symbol1 + item[2:]))
 
+# Make a list of coordinates
 for i in range(0, len(numbers), 2):
     points.append(list((numbers[i], numbers[i + 1])))
 
@@ -41,66 +43,68 @@ for i in range(0, len(numbers), 2):
 while True:
 
     try:
-        # print('2')
-        address = str(input('Enter your location: \n'))  # Получаем интересующий нас адрес
-        locations = geolocator.geocode(address,
-                                       exactly_one=False)  # Создаем переменную, которая состоит из нужного нам адреса
-        locations_ua = [loc_ua for loc_ua in locations if 'Україна' in loc_ua.address]
+        # Type our location
+        address = str(input('Enter your location: \n'))
+        locations = geolocator.geocode(address, exactly_one=False)
+        size = len(locations)
 
-        size = len(locations_ua)
-        for i in range(size):
-            print(i + 1, locations_ua[i])
+        # Use it if there is only one match
         if size == 1:
-            location = locations_ua[i]
-            # break
+            location = locations[0]
+            print(f'Your location is: \n{location}')
+
+        # Offer to select if there is more than one match
         elif size > 1:
+            for i in range(size):
+                print(i + 1, locations[i])
             print('Enter the number of your place:')
             num = int(input())
-
-            location = locations_ua[num - 1]
+            location = locations[num - 1]
             print(f'Your location is: \n{location}')
-            # break
         else:
             print('!!! No matches. Try again!')
 
-        # print('3')
-
-        print(location.latitude, location.longitude)  # И теперь выводим GPS-координаты нужного нам адреса
-
+        # Display the coordinates of our location (latitude, longitude)
+        print(location.latitude, location.longitude)
         my_location = [location.latitude, location.longitude]
 
-        map = folium.Map(
+        # Create a Folium Map object
+        my_map = folium.Map(
             location=my_location,
             zoom_start=10
         )
 
+        # Initiate a red circle marker
         folium.CircleMarker(
             location=my_location,
             radius=4,
             color='red',
             fill_color='Red',
             fill_opacity=0.9
-        ).add_to(map)
+        ).add_to(my_map)
 
-        marker_cluster = MarkerCluster().add_to(map)
+        # Initiate a cluster of markers
+        marker_cluster = MarkerCluster().add_to(my_map)
 
+        # Add points to map
         for point in points:
             folium.Marker(
                 location=point,
             ).add_to(marker_cluster)
 
+            # Calculate the distance from my location to each point
             distance2point[points.index(point) + 1] = round(distance.distance(my_location, tuple(point)).km, 1)
 
+        # Launch a webpage with the map
         output_file = "map.html"
-        map.save(output_file)
-        webbrowser.open(output_file, new=2)  # open in new tab
+        my_map.save(output_file)
+        webbrowser.open(output_file, new=2)  # open map in new tab
 
         print('Number of locations in message:', len(points))
-
         print('Min distance:', min(distance2point.values()))
         sys.exit()
 
     except TypeError:
         print('!!! No matches. Try again!')
-    except geopy.exc.GeocoderUnavailable:  # requests.exceptions.ConnectionError:
+    except geopy.exc.GeocoderUnavailable:   # requests.exceptions.ConnectionError:
         print('Bad connect. Restart...')
